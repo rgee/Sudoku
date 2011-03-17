676c43e7752f703c853fde0c662bc01c446f9304
Ryan Gee <rgee@wesleyan.edu>
2011-03-16 22:15:00 -0400

    Fixed grid. Changed default value to 0 isntead of -1. Wrote one knowledge-refinement procedure. Documented heap and added clear function.

--- sudoku.js
+++ sudoku.js
@@ -1,13 +1,60 @@
+
+/* The sudoku "solver" itself. Responsible for knowledge representation as well
+   as action selection */
+function Solver(){
+	/* Binary max heap of sub-square objects. Sub-squares have an index
+	   representing their position on the board and an array of their filled
+	   numbers. The scoring function for the heap is the length of this array.
+
+	   The idea is that I always look for the most complete subsquares
+	   first when trying to find trivial solutions like a subsquare with 8 cells
+	   filled.
+	*/
+	this.subSquareHeap = new BinHeap(function(u){return u.data.length;});
+}
+
+Solver.prototype = {
+	/* Run through all knowledge-refinement and square-filling tactics once. */
+	solve: function(){
+	},
+	processSubSquares: function(){
+		this.subSquareHeap.clear();
+		var row = 0;
+		for(var col = 0; col < 3; col++){
+			for(var row = 0; row < 3; row++){
+				var data = new Array();
+				for(var x = 0; x < 3; ++x){
+					for(var y = 0; y < 3; ++y){
+						var current = gameBoard.data[(row*3) + y][(col*3) + x];
+						if(current) {
+							data.push(current);
+						}
+					}
+				}
+				var subObj = {'idx' : [col,row],
+							  'data' : data};
+				this.subSquareHeap.push(subObj);
+			}
+			
+		}
+		console.log(this.subSquareHeap);
+	},
+	/* Fill in a square on the grid and update internal representations */
+	fillSquare : function(row, col, val){
+		this.processSubSquares();
+	}
+};
+
 function Board(){
 	this.data = new Array(9);
 
 	for(var i = 0; i < 9; i++){
 		this.data[i] = new Array(9);
 		for(var j = 0; j < 9; j++){
-			this.data[i][j] = -1;
+			this.data[i][j] = 0;
 		}
 	}
-	console.log(this.data[0][8]);
+	console.log(this.data);
 }
 
 Board.prototype = {
@@ -15,11 +62,6 @@ Board.prototype = {
 		this.data[row][col] = val;
 	}
 };
-
-/* Globals. Ohnoes. */
-var gameBoard = new Board();
-var term = new Object();
-var context = new Object();
 
 /* Performs all solver actions for one solver iteration. */
 function update(){
@@ -32,17 +74,19 @@ function draw(proc){
 	proc.keyboardMode = false;
 
 	proc.mousePressed = function(){
-		this.selectedRow = Math.floor((9 * (this.mouseY / this.height)));
-		this.selectedCol = Math.floor((9 * (this.mouseX / this.width)));
+		this.selectedCol = Math.floor((9 * (this.mouseY / this.height)));
+		this.selectedRow = Math.floor((9 * (this.mouseX / this.width)));
 		this.keyboardMode = true;
-		
-		term.put('Row: ' + this.selectedRow + ' Col: ' + this.selectedCol);
 	};
 	
 	proc.keyPressed = function() {
 		if(this.keyboardMode){
-			gameBoard.fillSquare(this.selectedRow, this.selectedCol, key);
-			this.keyboardMode = false;	
+			var str = String.fromCharCode(this.key);
+			if(!(/\D/.test(str))){
+				gameBoard.fillSquare(this.selectedRow, this.selectedCol, str);	
+				solver.processSubSquares();
+			}
+			this.keyboardMode = false;
 		}
 		
 	};
@@ -61,13 +105,13 @@ function draw(proc){
 		context.font = 'italic 30px sans-serif';
 		context.fillStyle = '#000';
 		context.textBaseline = 'top';
-		
-		var i = 0;
-		var j = 0;
-		for(i; i < 9; i++){
-			for(j; j < 9; j++){
-				context.fillText(gameBoard.data[i][j], i * (this.width / 9) + (this.width / 18),
-				                                       j * (this.height / 9) + (this.height / 18));
+	
+		for(var x = 0; x < 9; x++){
+			for(var y = 0; y < 9; y++){
+				if(gameBoard.data[x][y]) {
+					context.fillText(gameBoard.data[x][y], x * (this.width / 9) + (this.width / 18),
+														   y * (this.height / 9) + (this.height / 18));
+				}
 			}
 		}
 	};
@@ -90,6 +134,13 @@ function draw(proc){
 		
 	};
 }
+
+/* Globals. Ohnoes. */
+var gameBoard = new Board();
+var term = new Object();
+var context = new Object();
+var solver = new Solver();
+
 jQuery(document).ready(function(){
 	term = new Terminal('.terminal', 800, 300);
 	$('.terminal').click(function(){ term.put('(right <= length && this.scorer(this.data[right]) > this.scorer(this.data[largest))'); });
@@ -99,4 +150,6 @@ jQuery(document).ready(function(){
 	var procInstance = new Processing(document.getElementById('board'), draw);
 	procInstance.size(800,600);
 	context = document.getElementById('board').getContext('2d');
+	
+	solver.processSubSquares();
 }); 
