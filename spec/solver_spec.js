e9beed61e10dad2b9381086d9c7199b9c6f417c1
Ryan Gee <rgee@wesleyan.edu>
2011-04-04 01:00:02 -0400

    Added dependencies for Screw-Unit. Wrote a few incomplete unit tests.

--- spec/solver_spec.js
+++ spec/solver_spec.js
@@ -1,1 +1,44 @@
-Screw.Unit(function(){ describe('solver', function(){ var slv; before(function(){ slv = Solver(GameBoard()); }); describe('#eliminatePairs', function(){ describe('when there is a pair', function(){ before(function(){ slv.internalRepr = [[[2,3],[2,3],[2,3,7],[],[],[],[],[],[]], [[],[],[],[],[],[],[],[],[]], [[],[],[],[],[],[],[],[],[]], [[],[],[],[],[],[],[],[],[]], [[],[],[],[],[],[],[],[],[]], [[],[],[],[],[],[],[],[],[]], [[],[],[],[],[],[],[],[],[]], [[],[],[],[],[],[],[],[],[]], [[],[],[],[],[],[],[],[],[]]]; }); it("eliminates possibilities conflicting with pairs", function(){ slv.eliminatePairs(); var target = [[[2,3],[2,3],[7],[],[],[],[],[],[]], [[],[],[],[],[],[],[],[],[]], [[],[],[],[],[],[],[],[],[]], [[],[],[],[],[],[],[],[],[]], [[],[],[],[],[],[],[],[],[]], [[],[],[],[],[],[],[],[],[]], [[],[],[],[],[],[],[],[],[]], [[],[],[],[],[],[],[],[],[]], [[],[],[],[],[],[],[],[],[]]]; expect(slv.internalRepr).to(array_eq, target); }); }); }); }); });
+Screw.Unit(function(){
+	describe('solver', function(){
+		var slv;
+		before(function(){
+			slv = Solver(GameBoard());
+		});
+		describe('#eliminatePairs', function(){
+			describe('when there is a pair', function(){
+				before(function(){
+					slv.internalRepr = [[[2,3],[2,3],[2,3,7],[],[],[],[],[],[]],
+								[[],[],[],[],[],[],[],[],[]],
+								[[],[],[],[],[],[],[],[],[]],
+								[[],[],[],[],[],[],[],[],[]],
+								[[],[],[],[],[],[],[],[],[]],
+								[[],[],[],[],[],[],[],[],[]],
+								[[],[],[],[],[],[],[],[],[]],
+								[[],[],[],[],[],[],[],[],[]],
+								[[],[],[],[],[],[],[],[],[]]];
+ 				});
+ 				it("eliminates possibilities conflicting with pairs", function(){
+ 					slv.eliminatePairs();
+					var target = [[[2,3],[2,3],[7],[],[],[],[],[],[]],
+									[[],[],[],[],[],[],[],[],[]],
+									[[],[],[],[],[],[],[],[],[]],
+									[[],[],[],[],[],[],[],[],[]],
+									[[],[],[],[],[],[],[],[],[]],
+									[[],[],[],[],[],[],[],[],[]],
+									[[],[],[],[],[],[],[],[],[]],
+									[[],[],[],[],[],[],[],[],[]],
+									[[],[],[],[],[],[],[],[],[]]];
+					expect(slv.internalRepr).to(array_eq, target);
+				});
+			});
+			describe('when there is no pair', function(){
+				before(function(){
+
+				});
+				it("does nothing", function(){
+
+				});
+			});
+		});
+	});
+});
